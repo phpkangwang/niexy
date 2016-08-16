e783bf541fe39e8dfc30c167cbaabcce6baf28d9
wangkang <wangkang@qq.com>
2016-08-16 08:47:25 +0800

    更换框架为ci

--- common/config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'adminEmail' => 'admin@example.com',
-    'supportEmail' => 'support@example.com',
-    'user.passwordResetTokenExpire' => 3600,
-    'AppID'=>'wxc78c73cf4012af64',
-    'AppSecret'=>'b5cb9f7a16b03f279cc2a0c60eba9d5b',
-];
